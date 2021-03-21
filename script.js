3b86cf6ce34ebb7f5081e1bb433dbdd6bf31627d
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-03-21 13:32:53 -0500

    added time slots

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ $(function () {});
 var today = moment().format("dddd, MMMM Do, YYYY");
 
 var now = moment().format("H A");
+
+/* Entries for Workday */
+var planWorkday = [
+    {time: "9AM", event: ""},
+    {time: "10AM", event: ""},
+    {time: "11AM", event: ""},
+    {time: "12PM", event: ""},
+    {}
+];
