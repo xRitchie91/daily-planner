59f1eb0d9e121a2824ab44bb323a1e86eef1d0dd
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-03-21 13:52:16 -0500

    added rows

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ var now = moment().format("H A");
 
 /* Entries for Workday */
 var planWorkday = [
-    {time: "9AM", event: ""},
-    {time: "10AM", event: ""},
-    {time: "11AM", event: ""},
-    {time: "12PM", event: ""},
-    {}
+    {time: "0900", event: ""},
+    {time: "1000", event: ""},
+    {time: "1100", event: ""},
+    {time: "1200", event: ""},
+    {time: "1300", event: ""},
+    {time: "1400", event: ""},
+    {time: "1500", event: ""},
+    {time: "1600", event: ""},
+    {time: "1700", event: ""},
 ];
+
+/* Current time and rows */
+function colorRow(time) {
+    var planNow = moment(now, "H A");
+    var planEntry = moment(now, "H A");
+    if (planNow.isBefore(planEntry) === true) {
+        return "future";
+    }
+}
